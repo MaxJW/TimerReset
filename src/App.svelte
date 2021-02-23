fee7e8213f02a9919b7cfd304ab6bb997175fc95
Max Wilson <max@lettingweb.com>
2021-02-23 15:12:08 +0000

    Added voting system for timer resets
    Requires majority of 6 votes, including indicator for how many votes a member currently has either way

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,34 @@
 <script>
 	import "normalize.css"; //Normalize CSS elements - https://necolas.github.io/normalize.css/
-	import { db } from "./firebase.js";
+	import firebase from "firebase/app";
+	import "firebase/firestore";
+	import { db, auth, googleProvider } from "./firebase.js";
+	import { authState } from "rxfire/auth";
 	import { collectionData } from "rxfire/firestore";
 	import { startWith } from "rxjs/operators";
 	import Timer from "./components/Timer.svelte";
 	import Leaderboard from "./components/Leaderboard.svelte";
 	import Total from "./components/Total.svelte";
+	import { onDestroy } from "svelte";
+
+	let user;
+	let voter;
+	const unsubscribe = authState(auth).subscribe((u) => {
+		user = u;
+		voter = db.collection("voters").doc(u.uid);
+		voter.set({
+			alistair: false,
+			blair: false,
+			daniel: false,
+			ewan: false,
+			max: false,
+			ross: false,
+			name: "",
+		});
+	});
+	function login() {
+		auth.signInWithPopup(googleProvider);
+	}
 
 	const query = db.collection("timers").orderBy("name");
 	const timers = collectionData(query, "id").pipe(startWith([]));
@@ -30,23 +53,110 @@
 						lastReset: newResetTime,
 						timesReset: resetIncrease,
 						longestResetSeconds: secondsSinceReset,
+						yesVotes: 0,
+						noVotes: 0,
+						votingActive: false,
 				  }
-				: { lastReset: newResetTime, timesReset: resetIncrease };
+				: {
+						lastReset: newResetTime,
+						timesReset: resetIncrease,
+						yesVotes: 0,
+						noVotes: 0,
+						votingActive: false,
+				  };
 		db.collection("timers").doc(id).update(newData);
+		resetVoting();
 	}
+
+	function beginVoting(event) {
+		const { id, reason } = event.detail;
+		let newData = { votingActive: true, voteReason: reason };
+		db.collection("timers").doc(id).update(newData);
+		addYesVote(event);
+	}
+
+	function addYesVote(event) {
+		const id = event.detail;
+		voter.get().then((doc) => {
+			if (doc.data()[id] == false) {
+				db.collection("timers")
+					.doc(id)
+					.update({
+						yesVotes: firebase.firestore.FieldValue.increment(1),
+					});
+				voter.update({
+					[id]: true,
+				});
+			}
+		});
+	}
+
+	function addNoVote(event) {
+		const id = event.detail;
+		voter.get().then((doc) => {
+			if (doc.data()[id] == false) {
+				db.collection("timers")
+					.doc(id)
+					.update({
+						noVotes: firebase.firestore.FieldValue.increment(1),
+					});
+				voter.update({
+					[id]: true,
+				});
+			}
+		});
+	}
+
+	function resetVoting(event) {
+		const id = event.detail;
+
+		let newData = {
+			yesVotes: 0,
+			noVotes: 0,
+			votingActive: false,
+			voteReason: "",
+		};
+		db.collection("timers").doc(id).update(newData);
+		db.collection("voters")
+			.get()
+			.then((querySnapshot) => {
+				querySnapshot.forEach((doc) => {
+					doc.ref.update({
+						[id]: false,
+					});
+				});
+			});
+	}
+
+	onDestroy(unsubscribe);
 </script>
 
-<Leaderboard {leaderboard} />
+{#if user}
+	<Leaderboard {leaderboard} />
 
-<div id="main" class="center-full">
-	<h1>Timer Reset Dashboard</h1>
-	<div class="w-100 grid">
-		{#each $timers as timer}
-			<Timer {...timer} on:reset={resetTimer} />
-		{/each}
+	<div id="main" class="center-full">
+		<h1>Timer Reset Dashboard</h1>
+		<div class="w-100 grid">
+			{#each $timers as timer}
+				<Timer
+					{...timer}
+					on:reset={resetTimer}
+					on:beginvote={beginVoting}
+					on:yesvote={addYesVote}
+					on:novote={addNoVote}
+					on:endvote={resetVoting}
+				/>
+			{/each}
+		</div>
+		<Total {leaderboard} />
 	</div>
-	<Total {leaderboard} />
-</div>
+{:else}
+	<div class="center-full h-100">
+		<button on:click={login} class="center-full">
+			Sign In with Google
+		</button>
+	</div>
+{/if}
 
 <style>
 	#main {
@@ -54,12 +164,12 @@
 		justify-content: space-around !important;
 	}
 
-    h1 {
-        font-size: 2rem;
-        font-family: "Righteous", cursive;
-        text-shadow: 0 1px 5px rgb(0 0 0 / 50%);
+	h1 {
+		font-size: 2rem;
+		font-family: "Righteous", cursive;
+		text-shadow: 0 1px 5px rgb(0 0 0 / 50%);
 		letter-spacing: 1.5px;
-    }
+	}
 
 	.grid {
 		display: grid;
--- src/components/Timer.svelte
+++ src/components/Timer.svelte
@@ -10,30 +10,100 @@
     export let lastReset; //Get current time when starting/resuming timer
     export let timesReset;
     export let longestResetSeconds;
+
+    export let votingActive;
+    export let yesVotes;
+    export let noVotes;
+    export let voteReason;
+
     export let name;
     export let id;
 
     let timer;
+    let oldResetCount = 0;
     let resetter = false;
     let alert = new Audio("./reset.mp3");
     alert.volume = 0.5;
     let initial = true;
-    let notification = true;
+    let votingStarted = false;
 
     onMount(() => {
         timer = lastReset.toDate();
-        initial = true;
+        oldResetCount = timesReset;
+        initial = votingActive ? false : true;
     });
+
+    function beginVoting() {
+        if (
+            !confirm(
+                "Are you sure you want to vote for a reset on " +
+                    name +
+                    "'s timer?"
+            )
+        ) {
+            return;
+        }
+        var reason = prompt(
+            "Enter a reason for " + name + "'s reset",
+            "LoL Rage"
+        );
+        if (reason != null) {
+            votingStarted = true;
+            dispatch("beginvote", { id, reason });
+        }
+    }
+
+    function voteAlert() {
+        alert.play();
+        if (Notification.permission === "granted") {
+            var newnotif = new Notification(
+                "Voting for resetting " + name + "'s timer has started!"
+            );
+        } else if (Notification.permission !== "denied") {
+            Notification.requestPermission().then(function (permission) {
+                if (permission === "granted") {
+                    var newnotif = new Notification(
+                        "Voting for resetting " + name + "'s timer has started!"
+                    );
+                }
+            });
+        }
+        votingStarted = false;
+    }
+
+    function votingEndedAlert() {
+        if (!initial) {
+            alert.play();
+            if (Notification.permission === "granted") {
+                var newnotif = new Notification(
+                    name + "'s timer was NOT reset"
+                );
+            } else if (Notification.permission !== "denied") {
+                Notification.requestPermission().then(function (permission) {
+                    if (permission === "granted") {
+                        var newnotif = new Notification(
+                            name + "'s timer was NOT reset"
+                        );
+                    }
+                });
+            }
+        } else {
+            initial = false;
+        }
+    }
+
+    const addYesVote = () => dispatch("yesvote", id);
+    const addNoVote = () => dispatch("novote", id);
+    const endVote = () => dispatch("endvote", id);
+
+    const reasonDisplay = () => window.alert(voteReason);
 
     //Reset a timer to 0, and store time in leaderboard
     function resetTimer() {
-        if (!confirm("Are you sure you want to reset " + name + "'s timer?")) {
-            return;
-        }
         let newResetTime = firebase.firestore.Timestamp.fromDate(new Date());
         let resetIncrease = timesReset + 1;
         let secondsSinceReset = currTime > longestResetSeconds ? currTime : 0;
-        notification = false;
+        votingStarted = false;
         dispatch("reset", {
             id,
             newResetTime,
@@ -43,34 +113,22 @@
     }
 
     function resetAlert() {
-        if (!initial) {
-            resetter = true;
-            alert.play();
-            setTimeout(() => {
-                resetter = false;
-            }, 1000);
+        resetter = true;
+        alert.play();
+        setTimeout(() => {
+            resetter = false;
+        }, 1000);
 
-            if (notification) {
-                if (Notification.permission === "granted") {
-                    var notification = new Notification(
+        if (Notification.permission === "granted") {
+            var newnotif = new Notification(name + " had their timer reset!");
+        } else if (Notification.permission !== "denied") {
+            Notification.requestPermission().then(function (permission) {
+                if (permission === "granted") {
+                    var newnotif = new Notification(
                         name + " had their timer reset!"
                     );
-                } else if (Notification.permission !== "denied") {
-                    Notification.requestPermission().then(function (
-                        permission
-                    ) {
-                        if (permission === "granted") {
-                            var notification = new Notification(
-                                name + " had their timer reset!"
-                            );
-                        }
-                    });
                 }
-            }
-            notification = true;
-        } else {
-            initial = false;
-            console.log(initial);
+            });
         }
     }
 
@@ -78,8 +136,25 @@
         .collection("timers")
         .doc(id)
         .onSnapshot((doc) => {
-            timer = doc.data().lastReset.toDate();
-            resetAlert();
+            if (votingActive && votingStarted) {
+                voteAlert();
+            } else if (votingActive) {
+                if (yesVotes + noVotes == 6 || yesVotes >= 4 || noVotes >= 4) {
+                    if (yesVotes >= 4) {
+                        resetTimer();
+                    } else {
+                        endVote();
+                    }
+                }
+            } else {
+                if (timesReset > oldResetCount) {
+                    oldResetCount = timesReset;
+                    timer = doc.data().lastReset.toDate();
+                    resetAlert();
+                } else {
+                    votingEndedAlert();
+                }
+            }
         });
 
     $: currTime =
@@ -97,16 +172,42 @@
     class="timer"
     style="--dpp: url(/images/{name}.png)"
     class:resetbs={resetter}
+    class:resetting={votingActive}
 >
-    <span class="time-name hide">{name}</span>
-    <span class="time-span pos-abs"
-        >{hours}:{padWithZeroes(minutes)}:{padWithZeroes(seconds)}</span
-    >
-    <button class="hide" type="button" on:click={resetTimer}>Reset</button>
+    <span class="time-name hide cdefault">{name}</span>
+    {#if !votingActive}
+        <span class="time-span pos-abs cdefault"
+            >{hours}:{padWithZeroes(minutes)}:{padWithZeroes(seconds)}</span
+        >
+        <button class="hide" type="button" on:click={beginVoting}
+            >Vote Reset</button
+        >
+    {:else}
+        <div class="cpointer" on:click={reasonDisplay}>
+            <span class="time-span">Reset?</span>
+            <div class="center-full flex-row">
+                {#each Array(yesVotes) as _}
+                    <div class="box yes" />
+                {/each}
+                {#each Array(noVotes) as _}
+                    <div class="box no" />
+                {/each}
+                {#each Array(6 - yesVotes - noVotes) as _}
+                    <div class="box unknown" />
+                {/each}
+            </div>
+        </div>
+        <div>
+            <button class="hide" type="button" on:click={addYesVote}>Yes</button
+            >
+            <button class="hide" type="button" on:click={addNoVote}>No</button>
+        </div>
+    {/if}
 </div>
 
 <style>
     .timer {
+        box-sizing: border-box;
         background-image: linear-gradient(
                 rgba(0, 0, 0, 0.3),
                 rgba(0, 0, 0, 0.3)
@@ -125,6 +226,7 @@
         box-shadow: 0px 0px 0px 0px #ff0000;
         transition: background-size 0.5s ease, z-index 5s step-end,
             box-shadow 0.9s ease;
+        overflow: hidden;
     }
 
     .timer:hover {
@@ -143,6 +245,10 @@
         transition: visibility 0.3s ease, opacity 0.3s ease;
     }
 
+    .resetting {
+        border: 10px solid red;
+    }
+
     .time-name {
         font-size: 2rem;
         font-family: "Righteous", cursive;
@@ -158,5 +264,32 @@
     .resetbs {
         box-shadow: 0px 0px 1px 1400px #ff0000 !important;
         transition: z-index 5s step-end, box-shadow 0.9s ease !important;
+    }
+
+    .flex-row {
+        flex-direction: row !important;
+    }
+
+    .box {
+        height: 17px;
+        width: 34px;
+        margin: 0;
+        border: 1px solid black;
+    }
+    .yes {
+        background-color: #189023;
+    }
+    .no {
+        background-color: #6e1a18;
+    }
+    .unknown {
+        background-color: #323233;
+    }
+
+    .cpointer {
+        cursor: pointer;
+    }
+    .cdefault {
+        cursor: default;
     }
 </style>
