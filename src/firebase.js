3cc01b24f48d19e217975c7a039724109cb10e73
Max Wilson <max@lettingweb.com>
2021-02-23 15:46:42 +0000

    Need to initialise analytics 🤦‍♂️

--- src/firebase.js
+++ src/firebase.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
   };
 
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 export const auth = firebase.auth();
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
